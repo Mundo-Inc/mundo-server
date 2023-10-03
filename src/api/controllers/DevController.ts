cf2f099dc5dafcd5d3ea43ff833851636a1b40d9
Adel <adelghaeinian@gmail.com>
2023-10-03 14:10:09 -0300

    better duplicate removal

--- src/api/controllers/DevController.ts
+++ src/api/controllers/DevController.ts
@@ -109,17 +109,34 @@ export async function devTests(
   }
 }
 
+function cleanAndSort(str: string) {
+  return str.split(" ").sort().join(" ").toLowerCase();
+}
+
+function areSimilar(str1: string, str2: string) {
+  const cleanedStr1 = cleanAndSort(str1);
+  const cleanedStr2 = cleanAndSort(str2);
+
+  return (
+    cleanedStr1 === cleanedStr2 ||
+    levenshtein.get(cleanedStr1, cleanedStr2) <= 2 ||
+    cleanedStr1.includes(cleanedStr2) ||
+    cleanedStr2.includes(cleanedStr1)
+  );
+}
+
 export async function fixPlaces(
   req: Request,
   res: Response,
   next: NextFunction
 ) {
   try {
+    let dupCount = 0;
     handleInputErrors(req);
-    let places = await Place.find();
-    for (const pp of places) {
-      //fixing State codes + country codes TODO: Fix for all other states and countries
-      let p = pp as IPlace;
+    // Fixing country codes:
+    /*
+    const placesCursor = Place.find().cursor();
+    for await (const p of placesCursor) {
       console.log("Fixing state/country code for " + p._id);
       if (p.location.country === "United States") p.location.country = "US";
       if (stateMapping[p.location.state.toLowerCase()]) {
@@ -127,10 +144,9 @@ export async function fixPlaces(
       }
       await p.save();
     }
-
-    let newPlaces = await Place.find();
-    for (const pp of newPlaces) {
-      let p = pp as IPlace;
+    */
+    const newPlacesCursor = Place.find().cursor();
+    for await (const p of newPlacesCursor) {
       //removing duplicates
       const nearbyDuplicates = await Place.find({
         "location.geoLocation": {
@@ -139,72 +155,50 @@ export async function fixPlaces(
               type: "Point",
               coordinates: p.location.geoLocation.coordinates,
             },
-            $maxDistance: 50,
+            $maxDistance: 30,
           },
         },
       });
-
-      // Filter by name similarity if required, using your levenshtein method or similar
-      const similarNamedPlaces = nearbyDuplicates.filter(
-        (nearbyPlace) => levenshtein.get(p.name, nearbyPlace.name) <= 2
+      const similarNamedPlaces = nearbyDuplicates.filter((nearbyPlace) =>
+        areSimilar(nearbyPlace.name, p.name)
       );
-
       if (similarNamedPlaces.length <= 1) {
         continue;
       } else {
-        const placeToKeep = similarNamedPlaces[0];
+        const placeToKeep = similarNamedPlaces.reduce((keep, currentPlace) => {
+          // Keep the place if it has 'scores.phantom'
+          if (currentPlace.scores && currentPlace.scores.phantom) {
+            return currentPlace;
+          }
+          // If 'keep' has 'scores.phantom', maintain it as is
+          if (keep.scores && keep.scores.phantom) {
+            return keep;
+          }
+          // Keep the place with a longer name
+          if (currentPlace.name.length > keep.name.length) {
+            return currentPlace;
+          }
+          // If all conditions fail, maintain 'keep' as is
+          return keep;
+        }, similarNamedPlaces[0]);
+
         const placesToRemove = similarNamedPlaces.filter(
-          (p) => p._id !== placeToKeep._id
+          (place) => String(place._id) !== String(placeToKeep._id)
         );
         for (const placeToRemove of placesToRemove) {
-          await CheckIn.updateMany(
-            { place: placeToRemove._id },
-            {
-              place: placeToKeep._id,
-            }
-          );
-          await Deal.updateMany(
-            { place: placeToRemove._id },
-            {
-              place: placeToKeep._id,
-            }
-          );
-          await PlaceFeature.updateMany(
-            { place: placeToRemove._id },
-            {
-              place: placeToKeep._id,
-            }
-          );
-          await Review.updateMany(
-            { place: placeToRemove._id },
-            {
-              place: placeToKeep._id,
-            }
-          );
-          await SystemRecommendation.updateMany(
-            { placeId: placeToRemove._id },
-            {
-              placeId: placeToKeep._id,
-            }
-          );
-          await User.updateMany(
-            { latestPlace: placeToRemove._id },
-            {
-              latestPlace: placeToKeep._id,
-            }
-          );
-          await UserFeature.updateMany(
-            { interactedPlaces: { $in: [placeToRemove._id] } },
-            { $pull: { interactedPlaces: placeToRemove._id } }
-          );
+          // Update or delete other documents referring to `placeToRemove` as needed
+          await updateReferences(placeToRemove, placeToKeep);
         }
-
+        console.log("--- " + placeToKeep.name);
         for (const placeToRemove of placesToRemove) {
+          console.log("DUP " + placeToRemove.name);
+          dupCount++;
           await Place.deleteOne({ _id: placeToRemove._id });
         }
       }
     }
-    console.log("Fixing Finished");
+
+    console.log("Fixing Finished (" + dupCount + "Duplicates removed)");
     res.status(StatusCodes.OK).json({
       success: true,
       data: {},
@@ -214,5 +208,35 @@ export async function fixPlaces(
   }
 }
 
+async function updateReferences(placeToRemove: IPlace, placeToKeep: IPlace) {
+  await CheckIn.updateMany(
+    { place: placeToRemove._id },
+    { place: placeToKeep._id }
+  );
+  await Deal.updateMany(
+    { place: placeToRemove._id },
+    { place: placeToKeep._id }
+  );
+  await PlaceFeature.updateMany(
+    { place: placeToRemove._id },
+    { place: placeToKeep._id }
+  );
+  await Review.updateMany(
+    { place: placeToRemove._id },
+    { place: placeToKeep._id }
+  );
+  await SystemRecommendation.updateMany(
+    { placeId: placeToRemove._id },
+    { placeId: placeToKeep._id }
+  );
+  await User.updateMany(
+    { latestPlace: placeToRemove._id },
+    { latestPlace: placeToKeep._id }
+  );
+  await UserFeature.updateMany(
+    { interactedPlaces: { $in: [placeToRemove._id] } },
+    { $pull: { interactedPlaces: placeToRemove._id } }
+  );
+}
 
 const categories: string[] = ["restaurant", "bar", "cafe"];
