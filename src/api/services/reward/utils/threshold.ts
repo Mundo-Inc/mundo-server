c4b8765a2faa5029b11a2f51ba774c691f930c32
Adel Ghaenian <adelghaeinian@gmail.com>
2024-01-08 20:16:02 -0400

    wip - sending trending places to friends

--- src/api/services/reward/utils/threshold.ts
+++ src/api/services/reward/utils/threshold.ts
@@ -5,4 +5,8 @@ export const thresholds = {
   MAX_CHECKIN_PER_PLACE: 5,
   MAX_REVIEW_PER_PLACE: 5,
   REACT_ROLL_THRESHOLD: 25,
+
+  //Trend Analysis Thresholds
+  TREND_HOURS_THRESHOLD: 24 * 7 * 4,
+  TREND_MIN_FOLLOWING_ACTIVITY: 4,
 };
