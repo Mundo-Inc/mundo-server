9e74f4938e83825f8f1b9380343006c79bc34e2a
Kia Abdi <thekiabdi@gmail.com>
2024-04-15 16:17:27 +0300

    Webhook security

--- src/api/controllers/WebhookController.ts
+++ src/api/controllers/WebhookController.ts
@@ -1,13 +1,14 @@
 import type { NextFunction, Request, Response } from "express";
 import { type ValidationChain } from "express-validator";
 import { StatusCodes } from "http-status-codes";
+import twilio from "twilio";
 
 import Conversation, { type IConversation } from "../../models/Conversation";
 import User, { type IUser } from "../../models/User";
+import { dStrings, dynamicMessage } from "../../strings";
 import { createError, handleInputErrors } from "../../utilities/errorHandlers";
 import logger from "../services/logger";
 import { NotificationsService } from "../services/notifications.service";
-import { dStrings, dynamicMessage } from "../../strings";
 
 export const conversationsWebhookValidator: ValidationChain[] = [];
 export async function conversationsWebhook(
@@ -17,6 +18,32 @@ export async function conversationsWebhook(
 ) {
   try {
     handleInputErrors(req);
+
+    const twilioSignature = req.headers["x-twilio-signature"] as string;
+    const twilioAuthToken = process.env.TWILIO_AUTH_TOKEN!;
+
+    if (!twilioSignature || !twilioAuthToken) {
+      throw createError(
+        "Access denied. Missing Twilio signature or auth token.",
+        StatusCodes.UNAUTHORIZED
+      );
+    }
+
+    if (
+      twilio.validateRequest(
+        twilioAuthToken,
+        twilioSignature,
+        "https://8dbd-88-243-151-148.ngrok-free.app/api/v1/webhooks/conversations",
+        req.body
+      )
+    ) {
+      console.log("Here");
+    } else {
+      throw createError(
+        "Access denied. Invalid Twilio signature.",
+        StatusCodes.UNAUTHORIZED
+      );
+    }
 
     const { EventType, Body, Author, ParticipantSid, ConversationSid } =
       req.body;
@@ -61,7 +88,7 @@ export async function conversationsWebhook(
             message: {
               notification: {
                 title: authorInfo.name,
-                body: Body,
+                body: Body || "New message",
               },
               data: {
                 link: `/chat/${ConversationSid}`,
