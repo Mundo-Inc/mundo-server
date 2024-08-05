8b8382dcfbafa264164a8159d56df7e7d8535ef6
Kia Abdi <thekiabdi@gmail.com>
2024-08-05 16:36:12 +0300

    Update index.ts

--- src/api/services/logger/index.ts
+++ src/api/services/logger/index.ts
@@ -2,7 +2,6 @@ import { env } from "../../../env.js";
 import buildDevLogger from "./dev-logger.js";
 import buildProdLogger from "./prod-logger.js";
 
-const logger =
-  env.NODE_ENV === "development" ? buildDevLogger() : buildProdLogger();
+const logger = buildDevLogger()
 
 export default logger;
